9bc2547d55b310584bc78ab8a02c803f4c8056af
Colin Shiung <CShiung@smwnbf.org>
2025-11-28 18:31:46 -0800

    Added repeated dijkstra, prints the map after done

--- main.py
+++ main.py
@@ -20,8 +20,35 @@ def create_rand_graph():
         print(f"{u}: {neighbors}")
     return graph
 
-def repeated_dijkstra(graph, source):
-    pass
+def repeated_dijkstra(graph):
+    map = {
+        vertex: list() for vertex in graph
+    }
+    for vertex in graph:
+        map[vertex] = dijkstra(graph, vertex)
+    return map
+
+def dijkstra(graph, source):
+    distances = {vertex: float('infinity') for vertex in graph}
+    distances[source] = 0
+    priority_queue = [(0, source)]
+
+    while priority_queue:
+        current_distance, current_vertex = heapq.heappop(priority_queue)
+
+        #skip if on worse path
+        if current_distance > distances[current_vertex]:
+            continue
+
+        #explore neighbors, update distances, add to priority queue
+        for neighbor, weight in graph[current_vertex].items():
+            distance = current_distance + weight
+
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(priority_queue, (distance, neighbor))
+
+    return distances
 
 def floyd_warshal(graph, source):
     pass
@@ -33,18 +60,21 @@ def rand_graph_testing(counter):
     source = 5 #can change source if u want, even can make it random
 
     start = time.perf_counter()
-    res = repeated_dijkstra(graph, source)
+    res = repeated_dijkstra(graph)
     end = time.perf_counter()
+    
+    for(key, value) in res.items():
+        print(f"From vertex {key}: {value}")
 
     print("\nRepeated Dijkstra's Algorithm")
-    print("Runtime: ", start-end, " seconds")
+    print("Runtime: ", end-start, " seconds")
 
     start = time.perf_counter()
     res = floyd_warshal(graph, source)
     end = time.perf_counter()
 
     print("\nFloyd-Warshal Algorithm")
-    print("Runtime: ", start-end, " seconds")
+    print("Runtime: ", end-start, " seconds")
     print("\n\n")
 
 def main():
